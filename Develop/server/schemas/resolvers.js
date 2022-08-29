396fc4856a4687f6056ce1f437315c00f1f1ec75
Shmuel Hoffman <snyh212@gmail.com>
2022-08-29 13:28:47 -0400

    moved resolvers code into resolvers and added typedef file content

---
+++ Develop/server/schemas/resolvers.js
@@ -0,0 +1,13 @@
+const { AuthenticationError } = require('apollo-server-express');
+const { User } = require('../models');
+const { signToken } = require('../utils/auth');
+
+const resolvers = {
+    Query: {
+        //user, users me
+    },
+    Mutation: {
+        //addUser, login, saveBook, removeBook
+    }
+}
+module.exports = resolvers;
--- Develop/server/schemas/typeDefs.js
+++ Develop/server/schemas/typeDefs.js
@@ -1,12 +1,49 @@
-const { AuthenticationError } = require('apollo-server-express');
-const { User } = require('../models');
-const { signToken } = require('../utils/auth');
+const { gql } = require('apollo-server-express');
 
-const resolvers = {
-    Query: {
-        //user, users me
-    },
-    Mutation: {
-        //addUser, login, saveBook, removeBook
-    }
+// Query, Mutatio, User, Book, Auth
+const typeDefs = gql`
+type Query {
+    users: [User]
+    me: User
 }
+
+type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(newBook: saveBookContent!): User
+    removeBook(bookId: String!): User
+}
+
+input saveBookContent {
+    authors: [String]
+    description: String
+    title: String!
+    bookId: String!
+    image: String
+    link: String
+}
+
+type User{
+    _id: ID!
+    username: String
+    email: String
+    password: String
+    bookCount: String
+    savedBooks: [Book]
+}
+
+type Book {
+    bookId: String!
+    authors: [String]
+    description: String!
+    title: String!
+    image: String
+    link: String
+}
+
+type Auth {
+    token: ID!
+    user: User
+}
+`
+module.exports = typeDefs;
