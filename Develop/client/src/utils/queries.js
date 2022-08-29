fd4d13d74f00e9f88897c7a77e1332a57ee9291b
Shmuel Hoffman <snyh212@gmail.com>
2022-08-29 14:40:27 -0400

    added queries and mutations files and added query data

---
+++ Develop/client/src/utils/queries.js
@@ -0,0 +1,21 @@
+import { gql } from "@apollo/client";
+
+export const GET_ME = gql`
+    query Query {
+        me {
+            _id
+            username
+            email
+            password
+            bookCount
+            savedBooks {
+                authors
+                description
+                bookId
+                image
+                link
+                title
+            }
+        }
+    }
+`;
